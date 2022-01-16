8dda4b5a323451aac4c5d321470e8011ca5ab8b8
Jacob Morales <jdmorales2@student.fullsail.edu>
2022-01-16 17:08:12 -0500

    proj: Section 5.1 and 5.2 in the guide - Changed main.cpp to include a sphere detection function for our rays, so that we know if a ray hits a sphere, and added a red color to all of the rays that hit the sphere

--- Source/main.cpp
+++ Source/main.cpp
@@ -4,13 +4,27 @@
 
 #include <stdio.h>
 
+bool hit_sphere(const point3& center, double radius, const ray& r)
+{
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminiant = b * b - 4 * a * c;
+    return (discriminiant > 0);
+
+}
+
 color3 ray_color(const ray& r) 
 {
+    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+    {
+        return color3(1, 0, 0);
+    }
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y + 1.0);
     return (1.0 - t) * color3(1.0, 1.0, 1.0) + t * color3(0.5, 0.7, 1.0);
 }
-
 
 int main(int argc, char** argv)
 {
