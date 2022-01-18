45bd4daf6694d28603725831cb8e8b84cdb2e948
Jacob Morales <jdmorales2@student.fullsail.edu>
2022-01-17 23:00:24 -0500

    build: Added a timestamp to the output filename - Changed the windows-run.bat script to add the current date and time to the end of the output.ppm file originally created to help with comparing between images later

--- DevOps/windows-run.bat
+++ DevOps/windows-run.bat
@@ -1,9 +1,14 @@
 @echo off
 
+for /F "tokens=2" %%i in ('date /t') do set mydate=%%i
+set mytime=%time::=%
+set mytime=%mytime:.=%
+set mydate=%mydate:/=%
+
 cd ../
 
 if not exist "output" mkdir output
 
-build\VisualStudio\Release\rtiow.exe > output/output.ppm
+build\VisualStudio\Release\rtiow.exe > output/output%mydate%%mytime%.ppm
 
 pause
