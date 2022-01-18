2e8f7a7b7ea6a4b1700cdf8f7f752f5b3f204828
Jacob Morales <jdmorales2@student.fullsail.edu>
2022-01-17 22:16:14 -0500

    proj: Section 7.1 and 7.2 - Added camera.hpp to standardize the axis-aligned camera used in the demo - Added another ray color function to color.hpp that uses multi-sampling to have an anti-aliasing effect - Changed main to use the new camera class and sampling method - Changed the name of rtweekend.h to rtweekend.hpp to clarify that the file has more than just definitions - Changed CMakeLists.txt to better sectionize the source files and added the renamed rtweekend and the camera file

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,8 +1,9 @@
-#include "rtweekend.h"
+#include "rtweekend.hpp"
 
 #include "color.hpp"
 #include "hittable_list.hpp"
 #include "sphere.hpp"
+#include "camera.hpp"
 
 #include <stdio.h>
 
@@ -42,6 +43,7 @@ int main(int argc, char** argv)
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
 
     // World
 
@@ -51,14 +53,7 @@ int main(int argc, char** argv)
 
     // Camera
 
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
-
-    auto origin = point3(0, 0, 0);
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+    camera cam{};
 
     // Render
 
@@ -71,11 +66,14 @@ int main(int argc, char** argv)
 
         for (int i = 0; i < image_width; i++)
         {
-            auto u = double(i) / (image_width - 1);
-            auto v = double(j) / (image_height - 1);
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-            color3 pixel_color = ray_color(r, world);
-            write_color(std::cout, pixel_color);
+            color3 pixel_color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
+                ray r = cam.get_ray(u, v);
+                pixel_color += ray_color(r, world);
+            }
+            write_color(std::cout, pixel_color, samples_per_pixel);
         }
     }
 
