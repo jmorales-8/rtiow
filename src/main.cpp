9157017758596a233bfc3c78c7e737c8a8f58a54
Jacob Morales <jacobmorales2001@outlook.com>
2025-03-17 18:58:28 -0700

    refactor: move scene descriptions to hittable_list.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -25,68 +25,19 @@
 // External includes
 #include <argparse/argparse.hpp>
 
+void setup_args(int argc, char **argv, argparse::ArgumentParser &argparse);
+
 int main(int argc, char **argv)
 {
     using namespace jmrtiow;
 
     argparse::ArgumentParser argparser("rtiow", "0.1");
-
-    argparser.add_argument("--image-type", "-t")
-        .default_value(std::string{"png"})
-        .choices("png", "jpg", "jpeg", "bmp", "tga", "hdr", "ppm", "webp")
-        .nargs(1)
-        .help("The type of image to output [choices: png, jpg, jpeg, bmp, tga, hdr, ppm, webp]")
-        .metavar("TYPE");
-
-    argparser.add_argument("--filepath", "-f")
-        .default_value(std::string{"img.png"})
-        .help("The file location to output to")
-        .metavar("PATH")
-        .required();
-
-    try
-    {
-        argparser.parse_args(argc, argv);
-    }
-    catch (const std::exception &err)
-    {
-        std::cerr << err.what() << std::endl;
-        std::cerr << argparser;
-        std::exit(1);
-    }
+    setup_args(argc, argv, argparser);
 
     std::string filepath = argparser.get<std::string>("--filepath");
     std::string image_type_string = argparser.get("--image-type");
     image::image_type image_type_selection = image::image_type::Unknown;
-
-    if (image_type_string.compare("png") == 0)
-    {
-        image_type_selection = image::image_type::PNG;
-    }
-    else if (image_type_string.compare("jpg") == 0 || image_type_string.compare("jpeg") == 0)
-    {
-        image_type_selection = image::image_type::JPG;
-    }
-    else if (image_type_string.compare("bmp") == 0)
-    {
-        image_type_selection = image::image_type::BMP;
-    }
-    else if (image_type_string.compare("tga") == 0)
-    {
-        image_type_selection = image::image_type::TGA;
-    }
-    else if (image_type_string.compare("hdr") == 0)
-    {
-        image_type_selection = image::image_type::HDR;
-    }
-    else if (image_type_string.compare("ppm") == 0)
-    {
-        image_type_selection = image::image_type::PPM;
-    }
-    else if (image_type_string.compare("webp") == 0)
-    {
-        image_type_selection = image::image_type::WEBP;
-    }
+    std::string scene_type = argparser.get<std::string>("--scene");
 
     // Image
 
@@ -98,37 +49,13 @@ int main(int argc, char **argv)
 
     // World
 
-    auto world = jmrtiow::scene::random_scene();
-    // hittable_list world;
-
-#if 0   // First scene (three spheres and a green ground
-    auto material_ground = make_shared<scene::lambertian>(math::color3(0.8, 0.8, 0.0));
-    auto material_center = make_shared<scene::lambertian>(math::color3(0.1, 0.2, 0.5));
-    auto material_left  = make_shared<scene::dielectric>(1.5);
-    auto material_right = make_shared<scene::metal>(math::color3(0.8, 0.6, 0.2), 0.0);
-
-    world.add(make_shared<scene::sphere>(math::point3(0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(make_shared<scene::sphere>(math::point3(0.0, 0.0, -1.0), 0.5, material_center));
-    world.add(make_shared<scene::sphere>(math::point3(-1.0, 0.0, -1.0), 0.5, material_left));
-    world.add(make_shared<scene::sphere>(math::point3(-1.0, 0.0, -1.0), -0.45, material_left)); // bubble effect on the left scene::sphere.
-    world.add(make_shared<scene::sphere>(math::point3(1.0, 0.0, -1.0), 0.5, material_right));
-#elif 0 // Second scene (two spheres, blue and red)
-
-    auto R = cos(pi / 4);
-    auto material_left = make_shared<scene::lambertian>(math::color3(0, 0, 1));
-    auto material_right = make_shared<scene::lambertian>(math::color3(1, 0, 0));
-
-    world.add(make_shared<scene::sphere>(math::point3(-R, 0, -1), R, material_left));
-    world.add(make_shared<scene::sphere>(math::point3(R, 0, -1), R, material_right));
-#endif
-
-    // scene::camera cam(math::point3(-2, 2, 1), math::point3(0, 0, -1), math::vec3(0, 1, 0), 90, aspect_ratio);
-    // scene::camera cam(math::point3(-2, 2, 1), math::point3(0, 0, -1), math::vec3(0, 1, 0), 20, aspect_ratio);
-    // math::point3 lookfrom(3, 3, 2);
-    // math::point3 lookat(0, 0, -1);
-    // math::vec3 vup(0, 1, 0);
-    // auto dist_to_focus = (lookfrom - lookat).length();
-    // auto aperture = 2.0;
+    scene::hittable_list world;
+    if (scene_type.compare("demo2") == 0)
+        world = scene::demo_scene2();
+    else if (scene_type.compare("demo") == 0)
+        world = scene::demo_scene();
+    else
+        world = scene::random_scene();
 
     math::point3 lookfrom(13, 2, 3);
     math::point3 lookat(0, 0, 0);
@@ -155,9 +82,6 @@ int main(int argc, char **argv)
         {
             for (int j = image_height - 1; j >= 0; j--)
             {
-                //std::cerr << std::format("\rScanlines remaining: {} - {:.1f}%    ", j, 100.0 - (100.0f * (double(j) / (image_height - 1))));
-                //fflush(stderr);
-
                 for (int i = 0; i < image_width; i++)
                 {
                     math::color3 pixel_color(0, 0, 0);
@@ -177,13 +101,13 @@ int main(int argc, char **argv)
 
                     (*image_data_reference)[j * image_width + i] = pixel_color;
                 }
+
+                if (!*keep_working_reference)
+                {
+                    break;
+                }
             }
         } });
-
-    // std::cerr << "\nExporting to file...\n";
-    // image::image_exporter exporter;
-    // exporter.export_data(filepath, image_type_selection, image_data, image_width, image_height);
-    // std::cerr << "Done!\n";
 
     // Setup SDL
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0)
@@ -325,12 +249,12 @@ int main(int argc, char **argv)
         char *image_now;
         SDL_LockTexture(render_surface, NULL, (void **)&image_now, &stride);
 
-        int w, h;
-        unsigned int format;
-        SDL_QueryTexture(render_surface, &format, NULL, &w, &h);
-        std::cout << "Format is " << format << '\n';
-        std::cout << "Stride is " << stride << '\n';
-        std::cout << "Format type is " << SDL_PIXELTYPE(format) << '\n';
+        // int w, h;
+        // unsigned int format;
+        // SDL_QueryTexture(render_surface, &format, NULL, &w, &h);
+        // std::cout << "Format is " << format << '\n';
+        // std::cout << "Stride is " << stride << '\n';
+        // std::cout << "Format type is " << SDL_PIXELTYPE(format) << '\n';
 
         // Copy texture data to new texture
         for (size_t i = 0; i < image_height; i++)
@@ -346,6 +270,9 @@ int main(int argc, char **argv)
     }
 
     // Cleanup
+    keep_working = false;
+    work_thread.join();
+
     ImGui_ImplSDLRenderer2_Shutdown();
     ImGui_ImplSDL2_Shutdown();
     ImGui::DestroyContext();
@@ -355,4 +282,36 @@ int main(int argc, char **argv)
     SDL_Quit();
 
     return 0;
+}
+
+void setup_args(int argc, char **argv, argparse::ArgumentParser &argparser)
+{
+    argparser.add_argument("--image-type", "-t")
+        .default_value(std::string{"png"})
+        .choices("png", "jpg", "jpeg", "bmp", "tga", "hdr", "ppm", "webp")
+        .nargs(1)
+        .help("The type of image to output [choices: png, jpg, jpeg, bmp, tga, hdr, ppm, webp]")
+        .metavar("TYPE");
+
+    argparser.add_argument("--filepath", "-f")
+        .default_value(std::string{"img.png"})
+        .help("The file location to output to")
+        .metavar("PATH");
+
+    argparser.add_argument("--scene", "-s")
+        .default_value(std::string{"random"})
+        .choices("random", "demo", "demo2")
+        .help("The scene to render")
+        .metavar("SCENE");
+
+    try
+    {
+        argparser.parse_args(argc, argv);
+    }
+    catch (const std::exception &err)
+    {
+        std::cerr << err.what() << std::endl;
+        std::cerr << argparser;
+        std::exit(1);
+    }
 }
--- src/scene/hittable_list.hpp
+++ src/scene/hittable_list.hpp
@@ -132,6 +132,36 @@ namespace jmrtiow::scene
         auto t = 0.5 * (unit_direction.y + 1.0);
         return (1.0 - t) * math::color3(1.0, 1.0, 1.0) + t * math::color3(0.5, 0.7, 1.0);
     }
+
+    hittable_list demo_scene()
+    {
+        hittable_list world = hittable_list();
+        auto material_ground = make_shared<scene::lambertian>(math::color3(0.8, 0.8, 0.0));
+        auto material_center = make_shared<scene::lambertian>(math::color3(0.1, 0.2, 0.5));
+        auto material_left = make_shared<scene::dielectric>(1.5);
+        auto material_right = make_shared<scene::metal>(math::color3(0.8, 0.6, 0.2), 0.0);
+
+        world.add(make_shared<scene::sphere>(math::point3(0.0, -100.5, -1.0), 100.0, material_ground));
+        world.add(make_shared<scene::sphere>(math::point3(0.0, 0.0, -1.0), 0.5, material_center));
+        world.add(make_shared<scene::sphere>(math::point3(-1.0, 0.0, -1.0), 0.5, material_left));
+        world.add(make_shared<scene::sphere>(math::point3(-1.0, 0.0, -1.0), -0.45, material_left)); // bubble effect on the left scene::sphere.
+        world.add(make_shared<scene::sphere>(math::point3(1.0, 0.0, -1.0), 0.5, material_right));
+        return world;
+    }
+
+    hittable_list demo_scene2()
+    {
+        hittable_list world = hittable_list();
+
+        auto R = cos(pi / 4);
+        auto material_left = make_shared<scene::lambertian>(math::color3(0, 0, 1));
+        auto material_right = make_shared<scene::lambertian>(math::color3(1, 0, 0));
+
+        world.add(make_shared<scene::sphere>(math::point3(-R, 0, -1), R, material_left));
+        world.add(make_shared<scene::sphere>(math::point3(R, 0, -1), R, material_right));
+
+        return world;
+    }
 }
 
 #endif // SCENE_HITTABLE_LIST_HPP
